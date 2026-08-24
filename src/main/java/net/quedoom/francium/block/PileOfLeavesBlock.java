c513b75782ad6833af1d12785602d183bd478472
QueDoom <quedoom@totl.se>
2026-08-24 19:27:21 +0200

    Pile of Leaves Block

--- src/main/java/net/quedoom/francium/block/PileOfLeavesBlock.java
+++ src/main/java/net/quedoom/francium/block/PileOfLeavesBlock.java
@@ -1,17 +1,27 @@
 package net.quedoom.francium.block;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SegmentableBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.level.material.Fluid;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
+import net.quedoom.francium.init.ModItems;
 import net.quedoom.francium.init.ModProperties;
 import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.Nullable;
@@ -20,28 +30,35 @@ import java.util.Map;
 import java.util.function.Function;
 
 public class PileOfLeavesBlock extends Block {
-    int MIN_SEGMENT = 1;
-    int MAX_SEGMENT = 8;
+    int MIN_SEGMENT = 0;
+    int MAX_SEGMENT = 7;
     IntegerProperty AMOUNT = ModProperties.PILE_OF_LEAVES_SEGMENTS;
 
-    public PileOfLeavesBlock(Properties properties) {
+    public PileOfLeavesBlock(BlockBehaviour.Properties properties) {
         super(properties);
-        this.registerDefaultState(this.getStateDefinition().any().setValue(AMOUNT, 1));
+        this.registerDefaultState(this.getStateDefinition().any().setValue(AMOUNT, 0));
     }
 
     @Override
     public @Nullable BlockState getStateForPlacement(BlockPlaceContext context) {
-        BlockState state = context.getLevel().getBlockState(context.getClickedPos());
-        return this.defaultBlockState().setValue(AMOUNT, state.getValue(AMOUNT) + 1);
-    }
-
-    @Override
-    protected boolean canBeReplaced(BlockState state, BlockPlaceContext context) {
-        return state.getValue(AMOUNT) != 8;
+        return this.defaultBlockState();
     }
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
         builder.add(AMOUNT);
+    }
+
+    @Override
+    protected InteractionResult useItemOn(ItemStack itemStack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hitResult) {
+        int i = state.getValue(AMOUNT);
+        boolean isLeaf = itemStack.is(ModItems.LEAF);
+
+        if (isLeaf && i < MAX_SEGMENT) {
+//            itemStack.shrink(1);
+            level.setBlock(pos, state.setValue(AMOUNT, i + 1), 1);
+            return InteractionResult.CONSUME;
+        }
+        return InteractionResult.PASS;
     }
 }
---
+++ src/main/java/net/quedoom/francium/item/LeafItem.java
@@ -0,0 +1,9 @@
+package net.quedoom.francium.item;
+
+import net.minecraft.world.item.Item;
+
+public class LeafItem extends Item {
+    public LeafItem(Properties properties) {
+        super(properties);
+    }
+}
