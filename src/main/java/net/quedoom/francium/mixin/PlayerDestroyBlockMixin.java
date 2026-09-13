1603657afc60ad5c14282660664b27db0ee10928
QueDoom <norton@totl.se>
2026-09-13 21:28:02 +0200

    Made podzol drop dried leaves and be able to be hoed to farmland

--- src/main/java/net/quedoom/francium/mixin/PlayerDestroyBlockMixin.java
+++ src/main/java/net/quedoom/francium/mixin/PlayerDestroyBlockMixin.java
@@ -5,17 +5,20 @@ import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.tags.ItemTags;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.Containers;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.storage.loot.BuiltInLootTables;
+import net.quedoom.francium.Francium;
 import net.quedoom.francium.init.ModBlocks;
 import net.quedoom.francium.init.ModLootTables;
 import net.quedoom.francium.init.ModTags;
@@ -25,6 +28,8 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Random;
 
 @Mixin(Block.class)
 public class PlayerDestroyBlockMixin {
@@ -38,6 +43,16 @@ public class PlayerDestroyBlockMixin {
             at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/Block;dropResources(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/entity/BlockEntity;Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/item/ItemStack;)V")
     )
     private void dropDust(BlockState state, Level level, BlockPos pos, BlockEntity blockEntity, Entity breaker, ItemStack tool, Operation<Void> original) {
+        if (state.is(Blocks.OAK_LEAVES) || state.is(Blocks.DARK_OAK_LEAVES)) {
+            RandomSource random = level.getRandom();
+            int randi = random.nextInt(0, 100);
+            Francium.LOGGER.info("{}", randi);
+            if (randi < 1) {
+                level.setBlockAndUpdate(pos, ModBlocks.THICK_APPLE.defaultBlockState());
+            }
+            original.call(state, level, pos, blockEntity, ((Player) breaker), tool);
+        }
+
         if (state.is(ModTags.Blocks.DROPS_FORBIDDEN_DUST) || state.is(ModTags.Blocks.DROPS_FORBIDDEN_FLAKE) || state.is(ModTags.Blocks.SMALL_DROPS_FORBIDDEN_FLAKE)) {
             if (state.is(ModTags.Blocks.DROPS_FORBIDDEN_DUST)) {
                Block.dropResources(ModBlocks.SPECIAL_FORBIDDEN_DUST.defaultBlockState(), level, pos);
@@ -49,7 +64,7 @@ public class PlayerDestroyBlockMixin {
         }
 
         if (state.is(ModTags.Blocks.TILLS)) {
-            boolean blocks = state.is(Blocks.GRASS_BLOCK) || state.is(Blocks.ROOTED_DIRT) || state.is(Blocks.COARSE_DIRT);
+            boolean blocks = state.is(Blocks.GRASS_BLOCK) || state.is(Blocks.ROOTED_DIRT) || state.is(Blocks.COARSE_DIRT) || state.is(Blocks.PODZOL);
             if (tool.is(ItemTags.HOES)) {
                 level.setBlockAndUpdate(pos, Blocks.FARMLAND.defaultBlockState());
             } else {
@@ -57,7 +72,10 @@ public class PlayerDestroyBlockMixin {
                     level.setBlockAndUpdate(pos, Blocks.DIRT.defaultBlockState());
                 }
                 if (state.is(Blocks.GRASS_BLOCK)) {
-                    dropResources(Blocks.SHORT_GRASS.defaultBlockState(), level, pos);
+                    dropResources(Blocks.GRASS_BLOCK.defaultBlockState(), level, pos);
+                }
+                if (state.is(Blocks.PODZOL)) {
+                    dropResources(Blocks.PODZOL.defaultBlockState(), level, pos);
                 }
             }
         }
